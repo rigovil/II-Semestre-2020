d48f32eaa787c467d01c2b8e5af45c78b2fa09f9
Rodrigo <rodrigo.vilchez@hotmail.com>
2020-08-11 18:17:55 -0600

    Asistencias

--- README.md
+++
@@ -1,2 +0,0 @@
-# II-Semestre-2020
-CI-0120, CI-0121, CI-0122, CI-0123, Asistencias
---
+++ asistencias/progra1-g9/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String argv[]) {
+		int a = 1;
+		System.out.println(a);
+	}
+}
