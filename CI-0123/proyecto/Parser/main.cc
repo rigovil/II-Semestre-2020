b4c54225f1eaee6251c2607c299d010efa05e211
Rodrigo <rodrigo.vilchez@hotmail.com>
2020-09-01 23:06:33 -0600

    Parser PI

---
+++ CI-0123/proyecto/Parser/main.cc
@@ -0,0 +1,171 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fstream>
+#include <iostream>
+#include <thread>
+#include <semaphore.h>
+#include <sys/time.h>
+#include "Reader.h"
+#include "Parser.h"
+
+
+#define NUMBER_SERVERS  1               /* Valor utilizado para indicar el número de servidores a los que se les va a solicitar información. */
+#define FILE_NAME       "covid.txt"     /* Valor utilizado para indicar el nombre del archivo donde se encuentra el código fuente del servidor. */
+#define WORKERS         4               /* Valor utilizado para indicar la cantidad de trabajadores que van a analizar el código fuente de un servidor. */
+#define SEM_ON          1               /* Valor utilizado para indicar que un semáforo se inicializa con el valor de 1. */
+#define SEM_OFF         0               /* Valor utilizado para indicar que un semáforo se inicializa con el valor de 0. */
+#define PSHARED         0               /* Valor utilizado para indicar que los semáforos se comparten entre los hilos de un proceso. */
+#define ZERO            0               /* Valor utilizado en las estructuras de control if y for. */
+#define EXIT            0               /* Valor utilizado como parámetro para salir del programa en caso de error. */
+#define ERROR           -1              /* Valor utilizado para identificar un error en alguna función. */
+#define CR              "costa rica"    /* Valor utilizado para identificar a Costa Rica. */
+#define CA              "centroamerica" /* Valor utilizado para identificar la región centroamericana. */
+
+
+void serverReader(std::string, int);
+void serverParser(Reader *, Parser *, int);
+void covidData(std::string);
+
+
+/**
+ * Punto de entrada del programa al Parser, crea tantos hilos como solicitudes a servidores hayan,
+ * los elimina cuando estos hayan terminado su trabajo.
+ * @param argc cantidad de parámetros enviados por línea de comandos.
+ * @param argv array con strings que almacenan lo indicado por los parámetros.
+ * @return 0.
+ */
+
+int main(int argc, char ** argv) {
+
+    std::vector<std::string> server_address;
+    std::vector<int> workers;
+    
+    std::thread servers[NUMBER_SERVERS];
+    server_address.push_back(FILE_NAME);
+    workers.push_back(WORKERS);
+
+
+    /**
+     * 
+     * Aquí va el código que le pide al usuario si quiere ver la info de un cantón específico o de Costa Rica.
+     * Falta implementar el método covidData que es el que despliega la información de los datos.
+     * 
+     */
+
+
+    for(int i = ZERO; i < NUMBER_SERVERS; ++i) {
+
+        servers[i] = std::thread(serverReader, server_address[i], workers[i]);
+
+    }
+
+    for(int i = ZERO; i < NUMBER_SERVERS; ++i) {
+
+        servers[i].join();
+
+    }
+
+    return 0;
+
+}
+
+
+/**
+ * Se encarga de preparar los datos necesarios para solicitar información a un servidor en específico,
+ * solicita el código fuente de la dirección del servidor y crea los hilos para que distintos trabajadores
+ * encuentren la información relacionada con los datos del COVID-19 y sea desplegada al usuario.
+ * @param server_address dirección del servidor donde será sacada la información.
+ * @param workers cantidad de trabajadores que van a analizar el código fuente.
+ */
+
+void serverReader(std::string server_address, int workers) {
+
+    int i, result;
+    std::thread threads[workers];
+    std::vector<std::string>::iterator it;
+
+    sem_t *semaphores   = new sem_t[workers];
+    Parser *parser      = new Parser();
+    Reader *reader      = new Reader(server_address, workers, semaphores);
+
+    for(i = ZERO; i < workers; ++i) {
+
+        if(i == ZERO) {
+            result = sem_init(&semaphores[i], PSHARED, SEM_ON);
+        }
+        else {
+            result = sem_init(&semaphores[i], PSHARED, SEM_OFF);
+        }
+
+        if(ERROR == result) {
+            perror("main:serverReader:sem_init");
+            exit(EXIT);
+        }
+
+        threads[i] = std::thread(serverParser, reader, parser, i);
+
+    }
+
+    for(i = ZERO; i < workers; ++i) {
+
+        threads[i].join();
+        result = sem_destroy(&semaphores[i]);
+
+        if(ERROR == result) {
+            perror("main:serverReader:sem_destroy");
+            exit(EXIT);
+        }
+
+    }
+
+    std::cout << "\nINFORMACIÓN DEL SITIO \"" << server_address << "\"\n\n";
+
+    for(it = parser->regions.begin(); it != parser->regions.end(); ++it) {
+
+        std::cout << *it << "\n";
+
+    }
+
+    delete semaphores;
+    delete parser;
+    delete reader;
+
+}
+
+
+/**
+ * A partir de un trabajador en específico, solicita al reader las líneas del código fuente del servidor para ser analizadas.
+ * Este proceso se repite tantas veces mientras queden líneas disponibles por analizar del código fuente.
+ * @param reader puntero al objeto de la clase Reader que le asigna líneas a los trabajadores para que las analicen.
+ * @param parser puntero al objeto de la clase Parser que analiza una línea del código fuente para buscar la información solicitada del COVID-19.
+ * @param worker número de trabajador que solicita y analiza las líneas.
+ */
+
+void serverParser(Reader * reader, Parser * parser, int worker) {
+
+    std::string line;
+
+    while(reader->hasNext(worker)) {
+        line = reader->getNext(worker);
+        parser->readLine(line);
+    }
+
+}
+
+
+/**
+ * Muestra información del COVID-19 en la región solicitada por el usuario.
+ * @param region region solicitada por el usuario.
+ */
+
+void covidData(std::string region) {
+
+    if(region == CR) {
+        std::cout << "Datos del COVID-19 en Costa Rica:\n\n";
+    }
+    else {
+        std::cout << "Datos del COVID-19 en " << region << ":\n\n";
+    }
+
+}
---
+++ CI-0123/proyecto/Socket/Socket.cc
@@ -0,0 +1,261 @@
+#include "Socket.h"
+
+/**
+ * Crea el socket.
+ * @param stream tipo del socket.
+ * @param ipv6 protocol family del socket.
+ */
+
+Socket :: Socket( bool stream, bool ipv6 ) {
+
+   if(ipv6) {
+      if(stream) {
+         idSocket = socket(AF_INET6, SOCK_STREAM, 0);
+      }
+      else {
+         idSocket = socket(AF_INET6, SOCK_DGRAM, 0);
+      }
+   } 
+   else {
+      if(stream) {
+         idSocket = socket(AF_INET, SOCK_STREAM, 0);
+      }
+      else {
+         idSocket = socket(AF_INET, SOCK_DGRAM, 0);
+      }
+   }
+
+   this->is_ipv6 = ipv6;
+   
+   if(-1 == idSocket) {
+      perror("Socket::Socket");
+      exit(0);
+   }
+
+}
+
+
+/**
+ * Crea una instancia Socket a partir de un id que ya ha sido asignado.
+ * @param id el identificador del socket que ya ha sido creado.
+ */
+
+Socket :: Socket( int id ) {
+
+   this->idSocket = id;
+   this->is_ipv6 = false;
+
+}
+
+
+/**
+ * Destruye la instancia del socket.
+ */
+
+Socket :: ~Socket(){
+
+    Close();
+
+}
+
+
+/**
+ * Cierra y destruye el socket.
+ */
+
+void Socket :: Close(){
+
+   if(-1 == close(idSocket)) {
+      perror("Socket::Close");
+      exit(0);
+   }
+
+}
+
+
+/**
+ * Conecta el socket a partir de una dirección y un puerto.
+ * @param hostip string con la dirección del servidor.
+ * @param port número de puerto al cual se va a hacer la conexión.
+ * @return 0.
+ */
+
+int Socket :: Connect( const char * hostip, int port ) {
+
+   int conexion;
+
+   if(this->is_ipv6) {
+      struct sockaddr_in6 server_address6;
+      server_address6.sin6_flowinfo  = 0;
+      server_address6.sin6_family    = AF_INET6;
+      server_address6.sin6_port      = htons(port);
+      inet_pton(AF_INET6, hostip, &server_address6.sin6_addr);
+      conexion = connect(idSocket, (struct sockaddr *) &server_address6, sizeof(server_address6));
+   }
+   else {
+      struct sockaddr_in server_address;
+      server_address.sin_family   = AF_INET;
+      server_address.sin_port     = htons(port);
+      inet_pton(AF_INET, hostip, &server_address.sin_addr);
+      conexion = connect(idSocket, (struct sockaddr *) &server_address, sizeof(server_address));
+   }
+
+   if(-1 == conexion) {
+      perror("Socket::Connect");
+      exit(0);
+   }
+
+   return conexion;
+
+}
+
+
+/**
+ * Lee información enviada por un servidor a partir de un socket.
+ * @param text dirección de memoria donde se almacena el mensaje enviado por el servidor o el cliente.
+ * @param len cantidad máxima de bytes que se leen de la información enviada por el servidor o el cliente.
+ * @return la cantidad de bytes recibidos por el servidor o el cliente.
+ */
+
+int Socket :: Read( char * text, int len ) {
+
+   int r = read(idSocket, (void *) text, len);
+
+   if(-1 == r) {
+      perror("Socket::Read");
+      exit(0);
+   }
+
+   return r;
+
+}
+
+
+/**
+ * Envía información a un servidor o un ciente mediante un socket especificando el tamaño máximo del mensaje.
+ * @param text dirección de memoria donde se encuentra el mensaje a enviar.
+ * @param len cantidad máxima de bytes que se van a enviar.
+ * @return la cantidad de bytes enviados.
+ */
+
+int Socket :: Write( char * text, int len ) {
+
+   int w = write(idSocket, (const void *) text, len);
+
+   if(-1 == w) {
+      perror("Socket::Write");
+      exit(0);
+   }
+
+   return w;
+   
+}
+
+
+/**
+ * Envía información a un servidor o un cliente mediante un socket sin especificar el tamaño máximo del mensaje.
+ * @param text dirección de memoria donde se encuentra el mensaje a enviar.
+ * @return la cantidad de bytes enviados.
+ */
+
+int Socket :: Write( char * text ) {
+
+   int w = write(idSocket, (const void *) text, strlen((const char *) text));
+
+   if(-1 == w) {
+      perror("Socket::Write");
+      exit(0);
+   }
+
+   return w;
+
+}
+
+
+/**
+ * Marca un socket como pasivo pues será el que escucha por nuevas conexiones para aceptarlas.
+ * @param queue cantidad máxima de conexiones encoladas esperando ser atendidas por el servidor.
+ * @return 0.
+ */
+
+int Socket :: Listen( int queue ) {
+
+   int l = listen(idSocket, queue);
+
+   if(-1 == l) {
+      perror("Socket::Listen");
+      exit(0);
+   }
+
+   return l;
+
+}
+
+
+/**
+ * Le asigna y le asocia una dirección al socket.
+ * @param port número de puerto por el cual el socket va a estar asociado. 
+ * @return 0.
+ */
+
+int Socket :: Bind( int port ) {
+
+   struct sockaddr_in server_address;
+   server_address.sin_family      = AF_INET;
+   server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+   server_address.sin_port        = htons(port);
+
+   int b = bind(idSocket, (const struct sockaddr *) &server_address, sizeof(server_address));
+
+   if(-1 == b) {
+      perror("Socket::Bind");
+      exit(0);
+   }
+
+   return b;
+
+}
+
+
+/**
+ * Acepta la primera conexión pendiente de la cola que posee el socket que está haciendo listen,
+ * por lo que crea un nuevo socket para que esa conexión pendiente se comunique directamente con el servidor.
+ * @return una instancia de la clase Socket que posee el id retornado por el syscall accept.
+ * 
+ */
+
+Socket * Socket :: Accept() {
+
+   struct sockaddr_in server_address;
+   socklen_t server_address_size = sizeof(server_address);
+   
+   int a = accept(idSocket, (struct sockaddr *) &server_address, &server_address_size);
+
+   if(-1 == a) {
+      perror("Socket::Accept");
+      exit(0);
+   }
+
+   return new Socket(a);
+
+}
+
+
+/**
+ * Apaga una conexión asociada al socket.
+ * @param how define cómo se va apagar, si las recepciones o transmisiones se desactivan o no.
+ * @return 0.
+ */
+
+int Socket :: Shutdown( int how ) {
+
+   int s = shutdown(idSocket, how);
+
+   if(-1 == s) {
+      perror("Socket::Shutdown");
+      exit(0);
+   }
+
+   return s;
+
+}
